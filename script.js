39d718167dd2cab72813d8fab6ebc308128b2f30
David Ferrara <davidferrara@live.com>
2018-04-26 13:33:34 -0400

    Updated JavaScript Calculations

--- script.js
+++ script.js
@@ -1,30 +1,72 @@
-// Get the form from the html document.
-var theForm = document.forms["inputform"];
-// Get the rate information.
-var x = theForm.elements["digital-number"];
-var digitalUnitType = theForm.elements["digital-unit"];
+// Declare the global variables.
+var theForm, x, digitalUnitType, y, timeUnitType, total, totalUnitType;
 
-var y = theForm.elements["time-number"];
-var timeUnitType = theForm.elements["time-unit"];
+// Retrieves the information from the html document.
+function getFormInfo() {
+    // Get the form.
+    theForm = document.forms["inputform"];
 
-// Get the total amount needed to download.
-var total = theForm.elements["total-number"];
-var totalUnitType = theForm.elements["total-unit"]
+    // Get the rate information.
+    x = theForm.elements["digital-number"].value;
+    digitalUnitType = theForm.elements["digital-unit"].value;
 
-function calculate() {
-    var result = 0;
+    y = theForm.elements["time-number"].value;
+    timeUnitType = theForm.elements["time-unit"].value;
 
-    convertToSec(timeUnitType);
-
-    return result;
+    // Get the total amount needed to download.
+    total = theForm.elements["total-number"].value;
+    totalUnitType = theForm.elements["total-unit"].value;
 }
 
-function convertToSec(unit) {
+// Converts the time into seconds from minutes or hours.
+function convertToSec(unitType) {
     var result = 0;
     if (unitType == "minuite") {
         result = y * 60;
-    } else if () {
-
+    } else if (unitType == "hour") {
+        result = y * 120;
     }
+    return result;
+}
+
+// Converts the the digital units into megabytes from kilobytes or gigabytes.
+function convertToMB(unitType) {
+    var result = 0;
+    if (unitType == "kilobyte") {
+        result = x / 1024;
+    } else if (unitType == "gigabyte") {
+        result = x * 1024;
+    }
+    return result;
+}
+
+// Converts the total units into gigabytes.
+function convertToGB(unitType) {
+    var result = 0;
+    if (unitType == "kilobyte") {
+        result = total / 1024 / 1024;
+    } else if (unitType == "megabyte") {
+        result = total / 1024;
+    }
+    return result;
+}
+
+/*------------------------------------------------------------------
+Calculate the downloading time based on the total to be downnloaded
+and the rate at which it is being downloaded.
+-------------------------------------------------------------------- */
+function calculate() {
+    var result = 0;
+    getFormInfo();
+    x = convertToMB(digitalUnitType);
+    y = convertToSec(timeUnitType);
+    total = convertToGB(totalUnitType);
+
+    result = (y * total) / x;
+
+    alert("Rate: " + x + "MB/" + y + "SEC" + " Total: " + total);
+
+
+    document.getElementById("output").innerHTML = "Your expected download time is " + result;
     return result;
 }
