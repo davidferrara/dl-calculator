25dde80f4726adff60fe6f08e52a7f2a2778f030
David Ferrara <davidferrara@live.com>
2018-04-29 10:48:51 -0400

    Added a lot of stuff

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Declare the global variables.
-var theForm, x, digitalUnitType, y, timeUnitType, total, totalUnitType;
+var theForm, x, digitalUnitType, y, timeUnitType, total, totalUnitType, answer;
+//var reset = false;
 
 // Retrieves the information from the html document.
 function getFormInfo() {
@@ -16,48 +17,48 @@ function getFormInfo() {
     // Get the total amount needed to download.
     total = theForm.elements["total-number"].value;
     totalUnitType = theForm.elements["total-unit"].value;
+    alert("Rate: " + x + digitalUnitType + " / " + y + timeUnitType + "\nTotal: " + total + totalUnitType);
 }
 
 // Converts the time into seconds from minutes or hours.
-function convertToSec(unitType) {
-    var result = y;
+function convertToSec(unitType, num) {
+    var result = num;
     if (unitType == "minuite") {
-        result = y * 60;
+        result = num * 60;
     } else if (unitType == "hour") {
-        result = y * 120;
+        result = num * 120;
     }
     return result;
 }
 
 // Converts the the digital units into megabytes from kilobytes or gigabytes.
-function convertToMB(unitType) {
-    var result = x;
+function convertToMB(unitType, num) {
+    var result = num;
     if (unitType == "kilobyte") {
-        result = x / 1024;
+        result = num / 1024;
     } else if (unitType == "gigabyte") {
-        result = x * 1024;
+        result = num * 1024;
     }
     return result;
 }
 
+/*
 // Converts the total units into gigabytes.
-function convertToGB(unitType) {
-    var result = total;
+function convertToGB(unitType, num) {
+    var result = num;
     if (unitType == "kilobyte") {
-        result = total / 1024 / 1024;
+        result = num / 1024 / 1024;
     } else if (unitType == "megabyte") {
-        result = total / 1024;
+        result = num / 1024;
     }
     return result;
 }
+*/
 
+// Helper function for the setTimeout. Plays the ding sound.
 function playAudio() {
-    document.getElementById("ding-sound").play();
-}
-
-function display() {
-    var answer = calculate()
     document.getElementById("output").innerHTML = "Your expected download time is " + answer + "SEC";
+    document.getElementById("ding-sound").play();
 }
 
 /*------------------------------------------------------------------
@@ -67,16 +68,47 @@ and the rate at which it is being downloaded.
 function calculate() {
     var result = 0;
     getFormInfo();
-    x = convertToMB(digitalUnitType);
-    y = convertToSec(timeUnitType);
-    total = convertToGB(totalUnitType);
+    x = convertToMB(digitalUnitType, x);
+    y = convertToSec(timeUnitType, y);
+    total = convertToMB(totalUnitType, total);
 
     result = (y * total) / x;
     return result;
 }
 
+function timer() {
+    // Gets the future date when the DL is finished.
+    var d = new Date().getTime() + (answer * 1000);
+
+    // Update the timer every second
+    var a = setInterval(function() {
+        // Get the current date
+        var now = new Date().getTime();
+        // Amount of time between both times.
+        var distance = d - now;
+
+        // Calculate the time
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // output
+        document.getElementById("countdown").innerHTML = days + ":" + hours + ":" +
+            minutes + ":" + seconds;
+
+        if (distance < 0) {
+            clearInterval(a);
+            document.getElementById("countdown").innerHTML = "Your download should be finished by now.";
+            document.getElementById("alarm-sound").play();
+        }
+    }, 1000);
+}
+
 function begin() {
+    answer = calculate();
+    document.getElementById("output").innerHTML = "";
     document.getElementById("typing-sound").play();
     setTimeout(playAudio, 7000);
-    setTimeout(display, 7000);
+    setTimeout(timer, 6000);
 }
